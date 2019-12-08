20021300287a59a2e1455f3c4651145a74df8b8c
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2019-12-08 19:24:57 +0530

    updated main code

--- test-tf.py
+++ test-tf.py
@@ -3,26 +3,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-
 df_train = pd.read_csv('data-sets/sensor_data_cancer_train.csv')
-data_thp = df_train['100%_THP']   # cancerous cell
-data_pha = df_train['PHA(-)']     # non-cancerous cell
-
-data_cell = np.array([data_thp, data_pha])
-data_label = np.array([1, 0])
-
 df_test = pd.read_csv('data-sets/sensor_data_cancer_test.csv')
-data_test = np.array(df_test['datagot'])
+
+# cancerous cell
+data_thp_100 = df_train['100%_THP']
+data_thp_80 = df_train['80%_THP']
+data_thp_60 = df_train['60%_THP']
+data_thp_50 = df_train['50%_THP']
+data_thp_40 = df_train['40%_THP']
+data_thp_30 = df_train['30%_THP']
+data_thp_20 = df_train['20%_THP']
+data_thp_10 = df_train['10%_THP']
+
+# non-cancerous cell
+data_pha = df_train['PHA(-)']
+
+# test data
+data_got = df_test['datagot']
+
+
+data_cell = np.array([data_thp_100, data_thp_80, data_thp_60, data_thp_50, data_thp_40, data_thp_30, data_thp_20,
+                      data_thp_10, data_pha])
+data_label = np.array([1, 1, 1, 1, 1, 1, 1, 1, 0])
+data_test = np.array([data_got])
+
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(100, )),
+    tf.keras.layers.Dense(100, input_shape=(100, ), activation='relu'),
     tf.keras.layers.Dense(80, activation='sigmoid'),
-    tf.keras.layers.Dense(50, activation='relu'),
+    tf.keras.layers.Dense(50, activation='sigmoid'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 history = model.fit(data_cell, data_label, epochs=100)
+
+predictions = model.predict(data_test)
+
+print(np.argmax(predictions[0]))
 
 fig, axs = plt.subplots(2, 1)
 # plot loss
