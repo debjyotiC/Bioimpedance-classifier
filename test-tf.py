9d6380cdb9371408d524ee5a07a4a9ada5d677eb
debjyotiC <djbabai.debjyoti@gmail.com>
2019-12-26 14:33:33 +0530

    changed the activation function
    Signed-off-by: debjyotiC <djbabai.debjyoti@gmail.com>

--- test-tf.py
+++ test-tf.py
@@ -34,7 +34,7 @@ data_test = np.array([data_got])
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(100, input_shape=(100, ), activation='relu'),
-    tf.keras.layers.Dense(80, activation='relu'),
+    tf.keras.layers.Dense(80, activation='sigmoid'),
     tf.keras.layers.Dense(50, activation='sigmoid'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
