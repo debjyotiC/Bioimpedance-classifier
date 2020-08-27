78bfafeb3e64f6b906a23fbdbd3bd9bb701c749e
debjyotiC <djbabai.debjyoti@gmail.com>
2020-08-27 13:15:15 +0530

    new data and tf model added
    Signed-off-by: debjyotiC <djbabai.debjyoti@gmail.com>

---
+++ cell_data.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+volume_percentage = [1, 0.9, 0.7, 0.65, 0.5, 0.3, 0.15, 0.1]
+radius_cell = [3.1, 8]  # normal, thp-1
+imp_normal = []
+
+imp_thp_100 = []
+imp_thp_90 = []
+imp_thp_70 = []
+imp_thp_65 = []
+imp_thp_50 = []
+imp_thp_30 = []
+imp_thp_15 = []
+imp_thp_10 = []
+
+
+frequency = []
+
+
+def epsilon_mix(fraction, radius):
+    phi = (100 * fraction * pow(10, 6) * 4 / 3 * math.pi * radius ** 3) / (400 * pow(10, -6) * pow(10, -3))
+    e_mix = 80 * (1 + 2 * phi * (-0.49))/(1 - phi * (-0.49))
+    return e_mix
+
+
+def sensor_cap(epsilon_r, f):
+    cap = 8.85 * pow(10, -12) * epsilon_r * 1160 * pow(10, -6)
+    impedance = pow((2 * math.pi * f * pow(10, 3) * 400 * cap), -1)
+    return impedance
+
+
+for freq in range(1, 101, 1):
+    frequency.append(freq)
+    imp_normal.append(sensor_cap(epsilon_mix(volume_percentage[0], radius_cell[0] * pow(10, -6)), freq))
+    imp_thp_100.append(sensor_cap(epsilon_mix(volume_percentage[0], radius_cell[1] * pow(10, -6)), freq))
+    imp_thp_90.append(sensor_cap(epsilon_mix(volume_percentage[1], radius_cell[1] * pow(10, -6)), freq))
+    imp_thp_70.append(sensor_cap(epsilon_mix(volume_percentage[2], radius_cell[1] * pow(10, -6)), freq))
+
+    imp_thp_65.append(sensor_cap(epsilon_mix(volume_percentage[3], radius_cell[1] * pow(10, -6)), freq))
+
+    imp_thp_50.append(sensor_cap(epsilon_mix(volume_percentage[4], radius_cell[1] * pow(10, -6)), freq))
+    imp_thp_30.append(sensor_cap(epsilon_mix(volume_percentage[5], radius_cell[1] * pow(10, -6)), freq))
+
+    imp_thp_15.append(sensor_cap(epsilon_mix(volume_percentage[6], radius_cell[1] * pow(10, -6)), freq))
+
+    imp_thp_10.append(sensor_cap(epsilon_mix(volume_percentage[7], radius_cell[1] * pow(10, -6)), freq))
+
+
+
+# save performance data
+values = {'Frequency': frequency, '100_THP': imp_thp_100, '90_THP': imp_thp_90,
+          '70_THP': imp_thp_70, '65_THP': imp_thp_65, '50_THP': imp_thp_50, '30_THP': imp_thp_30, '15_THP': imp_thp_15,
+          '10_THP': imp_thp_10,
+          'PHA': imp_normal}
+df_w = pd.DataFrame(values, columns=['Frequency', '100_THP', '90_THP', '70_THP', '65_THP', '50_THP',
+                                     '30_THP', '15_THP', '10_THP', 'PHA'])
+df_w.to_csv("data-sets/data-gen.csv", index=None, header=True)
+
+
+plt.figure(1)
+plt.plot(frequency, imp_normal, '-k', linewidth=1, label='100 Normal')
+plt.plot(frequency, imp_thp_100, '-b', linewidth=1, label='100 THP')
+plt.plot(frequency, imp_thp_90, '-g', linewidth=1, label='90 THP')
+plt.plot(frequency, imp_thp_70, '-r', linewidth=1, label='70 THP')
+plt.plot(frequency, imp_thp_65, '-c', linewidth=1, label='65 THP')
+plt.plot(frequency, imp_thp_50, '-c', linewidth=1, label='50 THP')
+plt.plot(frequency, imp_thp_30, '-m', linewidth=1, label='30 THP')
+plt.plot(frequency, imp_thp_15, '-c', linewidth=1, label='15 THP')
+plt.plot(frequency, imp_thp_10, '-y', linewidth=1, label='10 THP')
+plt.xlabel('Frequency')
+plt.ylabel('Impedance')
+plt.legend(loc='best')
+plt.grid()
+plt.show()
+
--- test-bio.py
+++ test-bio.py
@@ -4,38 +4,46 @@ import tensorflow.compat.v1 as tf
 import matplotlib.pyplot as plt
 tf.disable_v2_behavior()
 
-df_train = pd.read_csv('data-sets/cleaned-data.csv')
+df_train = pd.read_csv('data-sets/data-gen.csv')
 
-data_thp_90 = df_train['90_THP']
-data_thp_10 = df_train['10_THP']
+data_thp_100 = df_train['100_THP']
+data_thp_30 = df_train['30_THP']
 
-test = df_train['20_THP']
+data_thp_10 = df_train['15_THP']
+data_pha_100 = df_train['PHA']
 
-data_train = np.array([data_thp_90, data_thp_10], dtype=float)
-data_label = np.array([0.0, 1.0], dtype=float)
+test = df_train['10_THP']
 
+data_train = np.array([data_thp_100, data_thp_30, data_thp_10, data_pha_100], dtype=float)
+data_label = np.array([0.0, 0.0, 1.0, 1.0], dtype=float)
 data_test = np.array([test], dtype=float)
 
+label_list = ['Cancerous', 'Non-cancerous']
+
 # parameters
-learning_rate = 0.2
+learning_rate = 0.3
 epochs = 20
 
 # data place holders
-input_data = tf.placeholder(tf.float32, [None, 11], name='input')
-output_data = tf.placeholder(tf.float32, [2, ], name='output')
+input_data = tf.placeholder(tf.float32, [None, 100], name='input')
+output_data = tf.placeholder(tf.float32, [4, ], name='output')
 
 # l0 layer
-w = tf.Variable(tf.random_normal([11, 11], stddev=0.03), name='w')
-b = tf.Variable(tf.random_normal([11]), name='b')
+input_no = 100
+w = tf.Variable(tf.random_normal([100, 80], stddev=0.03), name='w')
+b = tf.Variable(tf.random_normal([80]), name='b')
 output_nn_0 = tf.nn.softmax(tf.add(tf.matmul(input_data, w), b))
 
 # l1 layer
-w1 = tf.Variable(tf.random_normal([11, 7], stddev=0.03), name='w1')
-b1 = tf.Variable(tf.random_normal([7]), name='b1')
+neurons_l1 = 80
+w1 = tf.Variable(tf.random_normal([80, 50], stddev=0.03), name='w1')
+b1 = tf.Variable(tf.random_normal([50]), name='b1')
 output_nn_1 = tf.nn.softmax(tf.add(tf.matmul(output_nn_0, w1), b1))
 
-# l3 layer
-w2 = tf.Variable(tf.random_normal([7, 2], stddev=0.03), name='w2')
+# l2 layer
+neurons_l2 = 50
+output_no = 2
+w2 = tf.Variable(tf.random_normal([50, 2], stddev=0.03), name='w2')
 b2 = tf.Variable(tf.random_normal([2]), name='b2')
 output_nn = tf.nn.softmax(tf.add(tf.matmul(output_nn_1, w2), b2))
 
@@ -51,5 +59,8 @@ with tf.Session() as sess:
     sess.run(init_op)
     for epoch in range(epochs):
         _, c = sess.run([optimiser, error], feed_dict={input_data: data_train, output_data: data_label})
-        print("{:.5f}".format(c))
-    print(sess.run(tf.argmax(output_nn[0]), feed_dict={input_data: data_test}))
+        print("{}/{}========={:.5f}".format(epoch+1, epochs, c))
+    # print(sess.run(tf.argmax(output_nn, 1)[0], feed_dict={input_data: data_test}))
+    print(sess.run(tf.gather(label_list, tf.argmax(output_nn, 1)), feed_dict={input_data: data_test}))
+
+
