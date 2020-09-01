9d07adc4bbe494ff211a3f260eae72f7d78e29cf
debjyotiC <djbabai.debjyoti@gmail.com>
2020-09-01 21:15:40 +0530

    SVM classifier code added

--- bio_impedance_clf_knn.py
+++ bio_impedance_clf_knn.py
@@ -1,7 +1,8 @@
 import pandas as pd
-from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, precision_score, accuracy_score
+
 
 df = pd.read_csv('data-sets/three-stat.csv').dropna()\
     .reset_index(drop=True)
@@ -11,11 +12,11 @@ y = df['Label']
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=False, test_size=0.4)
 
-clf = svm.SVC()
-clf.fit(x_train, y_train)
-clf_output = clf.predict(x_test)
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(x_train, y_train)
+clf_output = neigh.predict(x_test)
 
 print(confusion_matrix(y_test, clf_output))
 
-print("SVM Precision score:{}".format(precision_score(y_test, clf_output)))
-print("SVM Accuracy score:{}".format(accuracy_score(y_test, clf_output)))
+print("KNN Precision score:{}".format(precision_score(y_test, clf_output)))
+print("KNN Accuracy score:{}".format(accuracy_score(y_test, clf_output)))
