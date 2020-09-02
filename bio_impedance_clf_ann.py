1054b2bb43ea1458fc182b59076b900dc8aa8aea
debjyotiC <djbabai.debjyoti@gmail.com>
2020-09-02 19:26:24 +0530

    ANN model with lambda layer added

--- bio_impedance_clf_ann.py
+++ bio_impedance_clf_ann.py
@@ -4,16 +4,22 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, precision_score, accuracy_score
 
-df = pd.read_csv('data-sets/three-stat.csv').dropna()\
-    .reset_index(drop=True)
+df = pd.read_csv('data-sets/three-stat.csv').dropna().reset_index(drop=True)
 
 x = df.drop(columns=['S No.', 'Value', 'Label'])
 y = df['Label']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=False, test_size=0.4)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=False, test_size=0.4, )
+
+
+def divide_by(sequence):
+    vfunc = np.vectorize(lambda t: t / (10 ** 5))
+    return vfunc(sequence)
+
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(100, input_dim=3, activation='relu'),
+    tf.keras.layers.Lambda(lambda t: t / (10 ** 5)),
+    tf.keras.layers.Dense(100, input_dim=3, activation='softmax'),
     tf.keras.layers.Dense(50, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
@@ -21,16 +27,16 @@ model = tf.keras.models.Sequential([
 model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
               optimizer=tf.keras.optimizers.Adam(learning_rate=0.1), metrics=['accuracy'])
 
-model_out = model.fit(x_train, y_train, epochs=50, validation_data=[x_test, y_test])
+model_out = model.fit(x_train, y_train, epochs=50)
 
 print("Training accuracy: {}".format(np.mean(model_out.history['accuracy'])))
-print("Validation accuracy: {}".format(np.mean(model_out.history['val_accuracy'])))
-
-y_prediction = model.predict(x_test)
-rounded = [round(x[0]) for x in y_prediction]
-y_cap_prediction = np.array(rounded, dtype='int64')
-
-print(confusion_matrix(y_test, y_cap_prediction))
-
-print("ANN Precision score:{}" .format(precision_score(y_test, y_cap_prediction)))
-print("ANN Accuracy score:{}".format(accuracy_score(y_test, y_cap_prediction)))
+# print("Validation accuracy: {}".format(np.mean(model_out.history['val_accuracy'])))
+#
+# y_prediction = model.predict(x_test)
+# rounded = [round(x[0]) for x in y_prediction]
+# y_cap_prediction = np.array(rounded, dtype='int64')
+#
+# print(confusion_matrix(y_test, y_cap_prediction))
+#
+# print("ANN Precision score:{}" .format(precision_score(y_test, y_cap_prediction)))
+# print("ANN Accuracy score:{}".format(accuracy_score(y_test, y_cap_prediction)))
