a7593213e11781c99d693a12acb79035bad11a33
debjyotiC <djbabai.debjyoti@gmail.com>
2020-09-03 09:56:51 +0530

    Lambda layer added to ANN

--- bio_impedance_clf_ann.py
+++ bio_impedance_clf_ann.py
@@ -6,17 +6,14 @@ from sklearn.metrics import confusion_matrix, precision_score, accuracy_score
 
 df = pd.read_csv('data-sets/three-stat.csv').dropna().reset_index(drop=True)
 
-x = df.drop(columns=['S No.', 'Value', 'Label']).to_numpy()
-y = df['Label'].to_numpy()
+x = df.drop(columns=['S No.', 'Value', 'Label']).to_numpy(dtype=float)
+y = df['Label'].to_numpy(dtype=float)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=False, test_size=0.4)
 
 
-def divide_by(sequence):
-    return np.vectorize(lambda t: t / (10 ** 5))(sequence)
-
-
 model = tf.keras.models.Sequential([
+    tf.keras.layers.Lambda(lambda seq: seq / (10 ** 5)),
     tf.keras.layers.Dense(100, input_dim=3, activation='softmax'),
     tf.keras.layers.Dense(50, activation='softmax'),
     tf.keras.layers.Dense(20, activation='relu'),
@@ -26,14 +23,13 @@ model = tf.keras.models.Sequential([
 model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
               optimizer=tf.keras.optimizers.Adam(learning_rate=0.1), metrics=['accuracy'])
 
-model_out = model.fit(divide_by(x_train), np.array(y_train), epochs=100,
-                      validation_data=[divide_by(x_test), np.array(y_test)])
 
+model_out = model.fit(x_train, y_train, epochs=200, validation_data=[x_test, y_test])
 
 print("Training accuracy: {:.5f}".format(np.mean(model_out.history['accuracy'])))
 print("Validation accuracy: {:.5f}".format(np.mean(model_out.history['val_accuracy'])))
 
-y_prediction = model.predict(divide_by(x_test))
+y_prediction = model.predict(x_test)
 rounded = [round(x[0]) for x in y_prediction]
 y_cap_prediction = np.array(rounded, dtype='int64')
 
