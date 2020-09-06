cb8e1f3390c998ff9fabb78aaa384d99c71daf75
debjyotiC <djbabai.debjyoti@gmail.com>
2020-09-06 20:36:10 +0530

    added 80 THP values
    Signed-off-by: debjyotiC <djbabai.debjyoti@gmail.com>

--- cell_data.py
+++ cell_data.py
@@ -3,10 +3,11 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-volume_percentage = [1, 0.9, 0.7, 0.65, 0.5, 0.3, 0.15, 0.1]
+volume_percentage = [1, 0.9, 0.8, 0.7, 0.65, 0.5, 0.3, 0.15, 0.1]
 radius_cell = [3.1, 8]  # normal, thp-1
 imp_normal_100 = []
 imp_normal_90 = []
+imp_normal_80 = []
 imp_normal_70 = []
 imp_normal_65 = []
 imp_normal_50 = []
@@ -17,6 +18,7 @@ imp_normal_10 = []
 
 imp_thp_100 = []
 imp_thp_90 = []
+imp_thp_80 = []
 imp_thp_70 = []
 imp_thp_65 = []
 imp_thp_50 = []
@@ -44,32 +46,33 @@ for freq in range(1, 101, 1):
     frequency.append(freq)
     imp_normal_100.append(sensor_cap(epsilon_mix(volume_percentage[0], radius_cell[0] * pow(10, -6)), freq))
     imp_normal_90.append(sensor_cap(epsilon_mix(volume_percentage[1], radius_cell[0] * pow(10, -6)), freq))
-    imp_normal_70.append(sensor_cap(epsilon_mix(volume_percentage[2], radius_cell[0] * pow(10, -6)), freq))
-    imp_normal_65.append(sensor_cap(epsilon_mix(volume_percentage[3], radius_cell[0] * pow(10, -6)), freq))
-    imp_normal_50.append(sensor_cap(epsilon_mix(volume_percentage[4], radius_cell[0] * pow(10, -6)), freq))
-    imp_normal_30.append(sensor_cap(epsilon_mix(volume_percentage[5], radius_cell[0] * pow(10, -6)), freq))
-    imp_normal_15.append(sensor_cap(epsilon_mix(volume_percentage[6], radius_cell[0] * pow(10, -6)), freq))
-    imp_normal_10.append(sensor_cap(epsilon_mix(volume_percentage[7], radius_cell[0] * pow(10, -6)), freq))
+    imp_normal_80.append(sensor_cap(epsilon_mix(volume_percentage[2], radius_cell[0] * pow(10, -6)), freq))
+    imp_normal_70.append(sensor_cap(epsilon_mix(volume_percentage[3], radius_cell[0] * pow(10, -6)), freq))
+    imp_normal_65.append(sensor_cap(epsilon_mix(volume_percentage[4], radius_cell[0] * pow(10, -6)), freq))
+    imp_normal_50.append(sensor_cap(epsilon_mix(volume_percentage[5], radius_cell[0] * pow(10, -6)), freq))
+    imp_normal_30.append(sensor_cap(epsilon_mix(volume_percentage[6], radius_cell[0] * pow(10, -6)), freq))
+    imp_normal_15.append(sensor_cap(epsilon_mix(volume_percentage[7], radius_cell[0] * pow(10, -6)), freq))
+    imp_normal_10.append(sensor_cap(epsilon_mix(volume_percentage[8], radius_cell[0] * pow(10, -6)), freq))
 
     imp_thp_100.append(sensor_cap(epsilon_mix(volume_percentage[0], radius_cell[1] * pow(10, -6)), freq))
     imp_thp_90.append(sensor_cap(epsilon_mix(volume_percentage[1], radius_cell[1] * pow(10, -6)), freq))
-    imp_thp_70.append(sensor_cap(epsilon_mix(volume_percentage[2], radius_cell[1] * pow(10, -6)), freq))
-    imp_thp_65.append(sensor_cap(epsilon_mix(volume_percentage[3], radius_cell[1] * pow(10, -6)), freq))
-    imp_thp_50.append(sensor_cap(epsilon_mix(volume_percentage[4], radius_cell[1] * pow(10, -6)), freq))
-    imp_thp_30.append(sensor_cap(epsilon_mix(volume_percentage[5], radius_cell[1] * pow(10, -6)), freq))
-    imp_thp_15.append(sensor_cap(epsilon_mix(volume_percentage[6], radius_cell[1] * pow(10, -6)), freq))
-    imp_thp_10.append(sensor_cap(epsilon_mix(volume_percentage[7], radius_cell[1] * pow(10, -6)), freq))
-
+    imp_thp_80.append(sensor_cap(epsilon_mix(volume_percentage[2], radius_cell[1] * pow(10, -6)), freq))
+    imp_thp_70.append(sensor_cap(epsilon_mix(volume_percentage[3], radius_cell[1] * pow(10, -6)), freq))
+    imp_thp_65.append(sensor_cap(epsilon_mix(volume_percentage[4], radius_cell[1] * pow(10, -6)), freq))
+    imp_thp_50.append(sensor_cap(epsilon_mix(volume_percentage[5], radius_cell[1] * pow(10, -6)), freq))
+    imp_thp_30.append(sensor_cap(epsilon_mix(volume_percentage[6], radius_cell[1] * pow(10, -6)), freq))
+    imp_thp_15.append(sensor_cap(epsilon_mix(volume_percentage[7], radius_cell[1] * pow(10, -6)), freq))
+    imp_thp_10.append(sensor_cap(epsilon_mix(volume_percentage[8], radius_cell[1] * pow(10, -6)), freq))
 
 
 # save performance data
-values = {'Frequency': frequency, '100_THP': imp_thp_100, '90_THP': imp_thp_90,
+values = {'Frequency': frequency, '100_THP': imp_thp_100, '90_THP': imp_thp_90, '80_THP': imp_thp_80,
           '70_THP': imp_thp_70, '65_THP': imp_thp_65, '50_THP': imp_thp_50, '30_THP': imp_thp_30, '15_THP': imp_thp_15,
-          '10_THP': imp_thp_10, '100_PHA': imp_normal_100, '90_PHA': imp_normal_90, '70_PHA': imp_normal_70,
+          '10_THP': imp_thp_10, '100_PHA': imp_normal_100, '90_PHA': imp_normal_90, '80_PHA': imp_normal_80, '70_PHA': imp_normal_70,
           '65_PHA': imp_normal_65, '50_PHA': imp_normal_50, '30_PHA': imp_normal_30, '15_PHA': imp_normal_15,
           '10_PHA': imp_normal_10}
-df_w = pd.DataFrame(values, columns=['Frequency', '100_THP', '90_THP', '70_THP', '65_THP', '50_THP',
-                                     '30_THP', '15_THP', '10_THP', '100_PHA', '90_PHA', '70_PHA', '65_PHA', '50_PHA',
+df_w = pd.DataFrame(values, columns=['Frequency', '100_THP', '90_THP', '80_THP', '70_THP', '65_THP', '50_THP',
+                                     '30_THP', '15_THP', '10_THP', '100_PHA', '90_PHA', '80_PHA', '70_PHA', '65_PHA', '50_PHA',
                                      '30_PHA', '15_PHA', '10_PHA'])
 df_w.to_csv("data-sets/data-gen.csv", index=None, header=True)
 
