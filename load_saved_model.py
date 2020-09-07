c8e93799b08e611fd73548f51a7837f82ab9a022
debjyotiC <djbabai.debjyoti@gmail.com>
2020-09-07 11:36:22 +0530

    added TF lite interpreter
    Signed-off-by: debjyotiC <djbabai.debjyoti@gmail.com>

--- load_saved_model.py
+++ load_saved_model.py
@@ -1,12 +1,27 @@
 import tensorflow as tf
 import numpy as np
 
-x_test = [502692.0591, 5026.920591, 26076.53486]
+x_test = np.array([502692.0591, 5026.920591, 26076.53486], dtype='float32')
 
-load_model = tf.keras.models.load_model('saved_model/tf_model')
+# load Keras model
+# load_model = tf.keras.models.load_model('saved_model/tf_model')
+# test = np.vstack([x_test])
+# classes = load_model.predict(test)
+
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="saved_model/tflite_model/converted_model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 test = np.vstack([x_test])
-classes = load_model.predict(test)
+interpreter.set_tensor(input_details[0]['index'], test)
+
+interpreter.invoke()
+
+classes = interpreter.get_tensor(output_details[0]['index'])
 
 if classes[0] > 0.5:
     print("is Cancerous")
